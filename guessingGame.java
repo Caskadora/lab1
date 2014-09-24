c5036956562057149f47501d0f9533d18591f1fd
Andy Wagner <andywagner@new.rr.com>
2014-09-24 14:16:00 -0500

    Infinite guesses
    Can now guess more than once.

--- guessingGame.java
+++ guessingGame.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main
+public class guessingGame
 {
 	public static void main(String args[])
 	{
@@ -11,15 +11,21 @@ public class Main
 		//number for the player to guess
 		int numberToGuess = r.nextInt(10);
 
-		System.out.println("Guess an integer between 0 and 10!");
-		int myInt = s.nextInt();
-		if(myInt == numberToGuess)
+		int myInt = -1;
+		
+		while(myInt != numberToGuess)
 		{
-			System.out.println("You got it!");
-		}
-		else
-		{
-			System.out.println("Not quite... the number was " + numberToGuess);
+			System.out.println("Guess an integer between 0 and 10!");
+			myInt = s.nextInt();
+			if(myInt == numberToGuess)
+			{
+				System.out.println("You got it!");
+				break;
+			}
+			else
+			{
+				System.out.println("Not quite... guess again!");
+			}
 		}
 
 	}
